1c6653bee26df3b79e7878d5d8e9009487a646e5
Martin Sinner <m-sinner@web.de>
2025-03-06 13:36:11 +0100

    fixed architecture & added background

---
+++ models/background-object.class.js
@@ -0,0 +1,10 @@
+class BackgroundObject extends MovableObject {
+
+
+    constructor(){
+        super().loadImage('img/5_background/layers/1_first_layer/1.png');
+        this.y = 250;
+        this.width = 400;
+        this.height = 250;
+    }
+}
