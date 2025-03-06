a0d9cbd9ba9fe486e8358d5b771f7552be7720a8
Martin Sinner <m-sinner@web.de>
2025-03-06 19:40:30 +0100

    added other layer backgrounds & optimized character & chicken size

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,10 +1,13 @@
 class BackgroundObject extends MovableObject {
+    width = 720;
+    height = 480;
 
 
-    constructor(){
-        super().loadImage('img/5_background/layers/1_first_layer/1.png');
-        this.y = 250;
-        this.width = 400;
-        this.height = 250;
+    constructor(imgPath, x) {
+        super().loadImage(imgPath);
+        this.x = x;
+        this.y = 480 - this.height;
+
+
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,10 @@ class World {
         new Cloud()
     ];
     backgroundObject = [
-        new BackgroundObject()
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
     ]
     canvas;
     ctx;
@@ -23,10 +26,12 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+
+        this.addObjectsToMap(this.backgroundObject);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.backgroundObject);
+        
 
 
         let self = this;
