9c367de3ef498ab0d4e3a90e90b3541b6ba6c187
Martin Sinner <m-sinner@web.de>
2025-03-17 14:27:19 +0100

    stopped dead animation loop

--- models/character.class.js
+++ models/character.class.js
@@ -33,7 +33,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/5_dead/D-54.png',
         'img/2_character_pepe/5_dead/D-55.png',
         'img/2_character_pepe/5_dead/D-56.png',
-        'img/2_character_pepe/5_dead/D-57.png',
+        'img/2_character_pepe/5_dead/D-57.png'
     ];
 
     IMAGES_HURT = [
@@ -114,7 +114,10 @@ class Character extends MovableObject {
             }
 
             if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
+                if(!this.isDeadAnimation){
+                    this.playAnimation(this.IMAGES_DEAD);
+                    this.isDeadAnimation = true;
+                }
             } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
             } else if (this.isAboveGround()) {
